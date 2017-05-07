e46bf64f2f4465f2940b05732bae7b22de5a152a
EKo3t <ekostukevich@gmail.com>
2017-05-07 17:08:15 +0300

    Try to make compiled pt.2

---
+++ TemplateAuth/Models/UserInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TemplateAuth.Models
+{
+    [Table("UserInfo")]
+    public class UserInfo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
